10b482f7e78251937e783461e8e963d419bfb0f0
Josh Nussbaum <joshnuss@gmail.com>
2022-12-08 10:43:00 -0500

    Adds basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,73 @@
 <script>
-	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core';
-	import { spring } from 'svelte/motion';
+	import { Canvas, OrbitControls, T } from '@threlte/core';
 	import { degToRad } from 'three/src/math/MathUtils';
+	import Chair from './Chair.svelte'
+	import Scene from './Scene.svelte'
 
-	const scale = spring(1);
+	const colors = [
+		{ name: 'red', value: '#ffee58' },
+		{ name: 'blue', value: '#1a237e' },
+		{ name: 'black', value: '#222' },
+		{ name: 'white', value: '#e1e1e1' },
+	]
+
+	let selectedColor = colors[0]
+	let stretcher = false
+
+	function selectColor(color) {
+		selectedColor = color
+	}
+
+	function selectStretcher(enabled) {
+		stretcher = enabled
+	}
 </script>
 
-<div>
+<aside>
+	<h2>Colors</h2>
+	<ul class="colors">
+		{#each colors as color}
+			<li class:selected={color == selectedColor}>
+				<button
+					style:background-color={color.value}
+					on:click={() => selectColor(color)}
+					title={color.name}>
+				</button>
+			</li>
+		{/each}
+	</ul>
+
+	<h2>Stretcher</h2>
+	<ul class="stretcher-options">
+		<li class:selected={!stretcher}>
+			<button
+				on:click={() => selectStretcher(false)}
+				title="No stretcher">
+				<Scene color={selectedColor.value} stretcher={false}/>
+			</button>
+		</li>
+
+		<li class:selected={stretcher}>
+			<button
+				on:click={() => selectStretcher(true)}
+				title="Stretcher">
+				<Scene color={selectedColor.value} stretcher={true}/>
+			</button>
+		</li>
+	</ul>
+</aside>
+
+<main>
 	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+		<T.PerspectiveCamera makeDefault position={[15, 15, 15]} fov={24}>
+			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={true} target={{ y: 0.5 }} />
 		</T.PerspectiveCamera>
 
 		<T.DirectionalLight castShadow position={[3, 10, 10]} />
 		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
 		<T.AmbientLight intensity={0.2} />
 
-		<!-- Cube -->
-		<T.Group scale={$scale}>
-			<T.Mesh position.y={0.5} castShadow let:ref>
-				<!-- Add interaction -->
-				<InteractiveObject
-					object={ref}
-					interactive
-					on:pointerenter={() => ($scale = 2)}
-					on:pointerleave={() => ($scale = 1)}
-				/>
-
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
-			</T.Mesh>
-		</T.Group>
+		<Chair color={selectedColor.value} {stretcher}/>
 
 		<!-- Floor -->
 		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
@@ -38,11 +75,48 @@
 			<T.MeshStandardMaterial color="white" />
 		</T.Mesh>
 	</Canvas>
-</div>
+</main>
 
 <style>
-	div {
+	main {
 		height: 100vh;
 		width: 100vw;
+	}
+
+	aside {
+		position: fixed;
+		display: flex;
+		flex-direction: column;
+		margin: 1rem;
+	}
+
+	aside h2 {
+		margin: 0;
+		padding: 0;
+		display: block;
+		color: #444;
+		font-size: 1rem;
+		font-family: Inter, sans;
+		font-weight: normal;
+	}
+
+	aside ul {
+		display: flex;
+		list-style: none;
+		gap: 0.5rem;
+		padding: 0;
+	}
+
+	aside button {
+		background: none;
+		border: solid 1px transparent;
+		padding: 0;
+		border-radius: 3px;
+		width: 50px;
+		aspect-ratio: 1;
+	}
+
+	aside .selected button {
+		border-color: #666;
 	}
 </style>
---
+++ src/routes/Chair.svelte
@@ -0,0 +1,100 @@
+<script>
+	import { T } from '@threlte/core';
+	export let color
+	export let stretcher = true
+</script>
+
+<!-- Legs -->
+<T.Group>
+	<!-- Leg #1 -->
+	<T.Mesh position.x={-0.75} position.y={1.25} position.z={-0.75} castShadow>
+		<T.BoxGeometry args={[0.2,2.5,0.2]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<!-- Leg #2 -->
+	<T.Mesh position.x={1.25} position.y={1.25} position.z={-0.75} castShadow>
+		<T.BoxGeometry args={[0.2,2.5,0.2]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<!-- Leg #3 -->
+	<T.Mesh position.x={-0.75} position.y={1.25} position.z={1} castShadow>
+		<T.BoxGeometry args={[0.2,2.5,0.2]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<!-- Leg #4 -->
+	<T.Mesh position.x={1.25} position.y={1.25} position.z={1} castShadow>
+		<T.BoxGeometry args={[0.2,2.5,0.2]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<T.Group rotation.z={0.2} position.x={0.45} position.y={0.3}>
+		<!-- Back vertical #1 -->
+		<T.Mesh position.x={-0.75} position.y={3.5} position.z={1} castShadow>
+			<T.BoxGeometry args={[0.2,2.25,0.2]}/>
+			<T.MeshStandardMaterial {color} />
+		</T.Mesh>
+
+		<!-- Back vertical #2 -->
+		<T.Mesh position.x={-0.75} position.y={3.5} position.z={-0.75} castShadow>
+			<T.BoxGeometry args={[0.2,2.25,0.2]}/>
+			<T.MeshStandardMaterial {color} />
+		</T.Mesh>
+
+		<!-- Back horizontal #1 -->
+		<T.Mesh position.x={-0.75} position.y={3.5} position.z={0.2} castShadow>
+			<T.BoxGeometry args={[0.10,0.25,1.75]}/>
+			<T.MeshStandardMaterial {color} />
+		</T.Mesh>
+
+		<!-- Back horizontal #2 -->
+		<T.Mesh position.x={-0.75} position.y={4.5} position.z={0.2} castShadow>
+			<T.BoxGeometry args={[0.10,0.25,1.75]}/>
+			<T.MeshStandardMaterial {color} />
+		</T.Mesh>
+	</T.Group>
+
+	<!-- Seat -->
+	<T.Mesh position.x={0.25} position.y={2.5} position.z={0.125} castShadow>
+		<T.BoxGeometry args={[2.25,0.05,2]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<!-- Seat Side #1 -->
+	<T.Mesh position.x={0.25} position.y={2.35} position.z={-0.75} castShadow>
+		<T.BoxGeometry args={[2,0.25,0.10]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<!-- Seat Side #2 -->
+	<T.Mesh position.x={0.25} position.y={2.35} position.z={1} castShadow>
+		<T.BoxGeometry args={[2,0.25,0.10]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<!-- Seat Side #3 -->
+	<T.Mesh position.x={-0.75} position.y={2.35} position.z={0.2} castShadow>
+		<T.BoxGeometry args={[0.1,0.25,1.8]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	<!-- Seat Side #4 -->
+	<T.Mesh position.x={1.25} position.y={2.35} position.z={0.2} castShadow>
+		<T.BoxGeometry args={[0.1,0.25,1.8]}/>
+		<T.MeshStandardMaterial {color} />
+	</T.Mesh>
+
+	{#if stretcher}
+		<T.Mesh position.x={0.25} position.y={1} position.z={1} castShadow>
+			<T.BoxGeometry args={[2,0.15,0.15]}/>
+			<T.MeshStandardMaterial {color}/>
+		</T.Mesh>
+
+		<T.Mesh position.x={0.25} position.y={1} position.z={-0.75} castShadow>
+			<T.BoxGeometry args={[2,0.15,0.15]}/>
+			<T.MeshStandardMaterial {color} />
+		</T.Mesh>
+	{/if}
+</T.Group>
